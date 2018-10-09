6f4187a1c0a91e207b429737d40e52d0087f750a
Rruhid <38219617+Rruhid@users.noreply.github.com>
2018-10-09 16:01:23 +0400

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,7 +14,7 @@ class CategoryController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function __construct(){
-     $this->middleware('admin');
+      $this->middleware(['auth','admin']);
 
     }
     
